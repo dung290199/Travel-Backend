4328cf2730baf3e8ae418ed6e0bf19e8c4d1c29a
DangTrung24hdev <trungdang.24hdev@gmail.com>
2020-11-04 23:10:32 +0700

    add struct second

---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+module.exports={
+    HOST:"localhost",
+    USER:'root',
+    PASSWORD:'password',
+    DB:'travel_backend',
+    dialect:'mysql',
+    pool:{
+        max:5,
+        mon:0,
+        acquire:30000,
+        idle:10000
+    }
+}
